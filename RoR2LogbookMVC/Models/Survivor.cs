3e8f1b9a27a09a67479c3f1e9247f5b82ff86b8b
Daniel Bedggood <ddbedggood@gmail.com>
2019-04-26 23:00:03 +1200

    Add model validations

--- RoR2LogbookMVC/Models/Survivor.cs
+++ RoR2LogbookMVC/Models/Survivor.cs
@@ -1,14 +1,23 @@
-﻿namespace RoR2LogbookMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoR2LogbookMVC.Models
 {
     public class Survivor
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Icon { get; set; }
+        [Required]
         public float BaseMaxHealth { get; set; }
+        [Required]
         public float MaxHealthIncrease { get; set; }
+        [Required]
         public float BaseDamage { get; set; }
+        [Required]
         public float DamageIncrease { get; set; }
+        [Required]
         public float Speed { get; set; }
         public string Notes { get; set; }
     }
