0ea2ee4acf2b65ad859e0849ecba0dbe1a7c1483
dbedggood <ddbedggood@gmail.com>
2019-04-28 20:29:07 +1200

    Remove hardcoded username and password

--- RoR2LogbookMVC/Controllers/AccountController.cs
+++ RoR2LogbookMVC/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -22,7 +23,7 @@ namespace RoR2LogbookMVC.Controllers
                 return RedirectToAction("Login");
             }
 
-            if (userName == "admin" && password == "pass")
+            if (userName == Environment.GetEnvironmentVariable("USERNAME") && password == Environment.GetEnvironmentVariable("SECRET"))
             {
 
                 //Create the identity for the user  
