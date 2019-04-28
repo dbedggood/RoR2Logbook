37d3cecce0b690bca545f7ef7e80a2dd7a85221f
dbedggood <ddbedggood@gmail.com>
2019-04-28 21:16:57 +1200

    Update Seed.cs

--- RoR2LogbookMVC/Models/Seed.cs
+++ RoR2LogbookMVC/Models/Seed.cs
@@ -27,26 +27,7 @@ namespace RoR2LogbookMVC.Models
                         Icon = "../../images/tier1/Syringe.png",
                         Name = "Soldier's Syringe",
                         Description = "Increases attack speed by 15% (+15% per stack).",
-                        Type = "tier1",
-                        Notes = "make you go fast zoom zoom haha lol"
-                    },
-
-                    new Item
-                    {
-                        Icon = "../../images/lunar/LunarDagger.png",
-                        Name = "Shaped Glass",
-                        Description = "Increase base damage by 100% (+100% per stack). Reduce maximum health by 50% (+50% per stack).",
-                        Type = "lunar",
-                        Notes = "make you do big damage but sometimes you die"
-                    },
-
-                    new Item
-                    {
-                        Icon = "../../images/equipment/Scanner.png",
-                        Name = "Radar Scanner",
-                        Description = "Reveal all interactables within 500m for 10 seconds.",
-                        Type = "equipment",
-                        Notes = "make you find loot good"
+                        Type = "tier1"
                     }
                 );
 
@@ -60,20 +41,8 @@ namespace RoR2LogbookMVC.Models
                         BaseDamage = 12f,
                         DamageIncrease = 2.4f,
                         Speed = 7f,
-                        Notes = "this guy kinda boring"
-                    },
-
-                    new Survivor
-                    {
-                        Icon = "../../images/survivor/Engi.png",
-                        Name = "Engineer",
-                        BaseMaxHealth = 130f,
-                        MaxHealthIncrease = 39f,
-                        BaseDamage = 14f,
-                        DamageIncrease = 2.8f,
-                        Speed = 7f,
-                        Notes = "super mega ultra chad"
                     }
+
                 );
 
                 context.SaveChanges();
