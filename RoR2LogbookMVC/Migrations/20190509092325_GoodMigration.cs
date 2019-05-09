8516de91cc4086d3a4451534e7e5c922880ce40c
Daniel Bedggood <ddbedggood@gmail.com>
2019-05-09 21:27:14 +1200

    Simplify migrations and update online database

--- RoR2LogbookMVC/Migrations/20190509092325_GoodMigration.cs
+++ RoR2LogbookMVC/Migrations/20190509092325_GoodMigration.cs
@@ -3,23 +3,43 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace RoR2LogbookMVC.Migrations
 {
-    public partial class Survivor : Migration
+    public partial class GoodMigration : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Icon = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Type = table.Column<string>(nullable: false),
+                    PickupText = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: false),
+                    Challenge = table.Column<string>(nullable: true),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.ID);
+                });
+
             migrationBuilder.CreateTable(
                 name: "Survivor",
                 columns: table => new
                 {
                     ID = table.Column<int>(nullable: false)
                         .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Name = table.Column<string>(nullable: true),
-                    Icon = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    Icon = table.Column<string>(nullable: false),
                     BaseMaxHealth = table.Column<float>(nullable: false),
                     MaxHealthIncrease = table.Column<float>(nullable: false),
                     BaseDamage = table.Column<float>(nullable: false),
                     DamageIncrease = table.Column<float>(nullable: false),
                     Speed = table.Column<float>(nullable: false),
+                    Challenge = table.Column<string>(nullable: true),
                     Notes = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
@@ -30,6 +50,9 @@ namespace RoR2LogbookMVC.Migrations
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Item");
+
             migrationBuilder.DropTable(
                 name: "Survivor");
         }
